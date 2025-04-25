959acc6a1bdb49a9d4047b60228ce6b6f94b331d
monicafebrii <192284635+monicafebrii@users.noreply.github.com>
2025-04-26 00:55:25 +0700

    prak8 storage-file

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,5 @@ Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update')
 Route::get('/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
 
 Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+
+Route::get('/', [UserController::class, 'index'])->name('user.list');
